4c1ef42ca579b49a909d20cedfbb707e7b054653
lukewberg <lukewberg@gmail.com>
2023-08-28 14:04:08 -0700

    Operator trait implpementations using SIMD

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use std::time::Instant;
 use raymarch::vec3::Vec3;
 
 fn main() {
-    let vecA = Vec3::new(2.34623342, 5.2983742, 9.12387978);
-    let vecB = Vec3::new(7.348756, 6.289734, 3.903457);
+    let vec_a = Vec3::new(2.34623342, 5.2983742, 9.12387978);
+    let vec_b = Vec3::new(7.348756, 6.289734, 3.903457);
 
     // let benchA = Instant::now();
     let mut i = 0;
     while i < 1_000_000_000 {
-        Vec3::multiply_vec3(&vecA, &vecB);
+        // Vec3::multiply_vec3(&vecA, &vecB);
         // Vec3::multiply_vec3_simd(&vecA, &vecB);
-        i += 1;   
+        vec_a.multiply_vec3_scalar(1245.567231412334);
+        vec_a.multiply_vec3_scalar_simd(1245.567231412334);
+        i += 1;
     }
     // let aResult = benchA.elapsed();
     // println!("Bench A: {:.2?}", aResult);
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::simd::f32x4;
+use std::{ops, simd::{f32x4, f32x64}};
 
 pub struct Vec3 {
     pub vec: [f32; 4],
@@ -28,9 +28,9 @@ impl Vec3 {
         let vecA = f32x4::from_array(a.vec);
         let vecB = f32x4::from_array(b.vec);
 
-        return Vec3 {
+        Vec3 {
             vec: (vecA * vecB).into(),
-        };
+        }
     }
 
     pub fn multiply_vec3(a: &Vec3, b: &Vec3) -> Vec3 {
@@ -39,5 +39,102 @@ impl Vec3 {
         result[1] = a.vec[1] * b.vec[1];
         result[2] = a.vec[2] * b.vec[2];
         Vec3 { vec: result }
+    }
+
+    pub fn multiply_vec3_scalar_simd(&self, scalar: f32) -> Vec3 {
+        let vec_vec = f32x4::from_array(self.vec);
+        let vec_scalar = f32x4::splat(scalar);
+        Vec3 {
+            vec: (vec_vec * vec_scalar).into(),
+        }
+    }
+
+    pub fn multiply_vec3_scalar(&self, scalar: f32) -> Vec3 {
+        Vec3 {
+            vec: [
+                self.vec[0] * scalar,
+                self.vec[1] * scalar,
+                self.vec[2] * scalar,
+                0.0,
+            ],
+        }
+    }
+
+    // pub fn batch_multiply_vec3_simd(vecs_a: [Vec3; ], vecs_b: [Vec3]) -> Vec<Vec3> {
+    //     Vec::new()
+    // }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        let vec_vec = f32x4::from_array(self.vec);
+        let vec_scalar = f32x4::splat(rhs);
+        Vec3 {
+            vec: (vec_vec * vec_scalar).into(),
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        let vec_self = f32x4::from_array(self.vec);
+        let vec_vec = f32x4::from_array(rhs.vec);
+
+        Vec3 {
+            vec: (vec_self * vec_vec).into(),
+        }
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, rhs: f32) -> Self::Output {
+        let vec_scalar = f32x4::splat(rhs);
+        let vec_vec = f32x4::from_array(self.vec);
+
+        Vec3 {
+            vec: (vec_scalar / vec_vec).into(),
+        }
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: Vec3) -> Self::Output {
+        let vec_self = f32x4::from_array(self.vec);
+        let vec_vec = f32x4::from_array(rhs.vec);
+
+        Vec3 {
+            vec: (vec_self / vec_vec).into(),
+        }
+    }
+}
+
+impl ops::Add<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: f32) -> Self::Output {
+        let vec_scalar = f32x4::splat(rhs);
+        let vec_vec = f32x4::from_array(self.vec);
+        Vec3 {
+            vec: (vec_scalar + vec_vec).into(),
+        }
+    }
+}
+
+impl ops::Sub<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: f32) -> Self::Output {
+        let vec_scalar = f32x4::splat(rhs);
+        let vec_vec = f32x4::from_array(self.vec);
+        Vec3 {
+            vec: (vec_scalar - vec_vec).into(),
+        }
     }
 }
